d9904775c3ed38c93635ed60863d68dd6fea4f8b
Jack Rickard <jack.rickard@softwire.com>
2018-07-20 14:03:55 +0100

    Part 2

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const request = require('request');
 const readlineSync = require('readline-sync');
 const log4js = require('log4js');
 const processors = require('./processors');
@@ -20,12 +19,7 @@ log4js.configure({
 const logger = log4js.getLogger('index.js');
 logger.info('Logging Initialised');
 
-const appID = 'fa99cefe';
-const appKey = 'b6282f249791ac9e502921e556779b8b';
-const arrivalsUrl = (stopCode) => `https://api.tfl.gov.uk/StopPoint/${stopCode}/Arrivals?app_id=${appID}&app_key=${appKey}`;
-
-readlineSync.setDefaultOptions({prompt: 'Enter the stop code: '})
-readlineSync.promptLoop((input) => request(arrivalsUrl(input), (error, response, body) => processors.printBuses(body)));
-
-
+readlineSync.setDefaultOptions({prompt: 'Enter the post code: '})
+readlineSync.promptLoop((input) => processors.printBusesNearPostcode(input));
+// readlineSync.promptLoop((input) => processors.printBusesForStopCode(input));
 
