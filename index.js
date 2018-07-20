246ce35a13dfbc38ffe675d55fe264d002b95768
Jack Rickard <jack.rickard@softwire.com>
2018-07-20 10:42:19 +0100

    Init

---
+++ index.js
@@ -0,0 +1,20 @@
+const request = require('request');
+const readlineSync = require('readline-sync');
+const log4js = require('log4js');
+
+log4js.configure({
+    appenders: {
+        file: {
+            type: 'fileSync',
+            filename: 'logs/debug.log'
+        }
+    },
+    categories: {
+        default: {
+            appenders: ['file'],
+            level: 'debug'
+        }
+    }
+});
+const logger = log4js.getLogger('index.js');
+logger.info('Logging Initialised');
